5cd39c91dc99f18bcf08bed6cfbdc6b88458077f
Harshit Kant <96339258+Harshitkant21@users.noreply.github.com>
2024-03-18 06:36:51 +0000

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,10 +18,15 @@ app.post("/bfhl", (req, res) => {
     user_id: "john_doe_17091999",
     email_id: "john@xyz.com",
     roll_number: "ABC123",
-    even_numbers: data.filter(num => parseInt(num) % 2 === 0),
-    odd_numbers: data.filter(num => parseInt(num) % 2 !== 0),
+    even_numbers: data.filter((num) => {
+      return parseInt(num) % 2 === 0;
+    }),
+    odd_numbers:data.filter((num) => {
+      return parseInt(num) % 2 !== 0;
+    }),
     alphabets: data.filter(str => /[A-Za-z]/.test(str)).map(str => str.toUpperCase())
   };
+
 
   res.json(response);
 });
